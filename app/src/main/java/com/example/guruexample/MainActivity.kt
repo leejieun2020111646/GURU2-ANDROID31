cf9197a883de33a6b5ba4619ae0a1dc9548b43b5
eyunseo <yunseo528@naver.com>
2023-01-26 03:34:41 +0900

    메인화면 버튼 클릭시 이동

--- app/src/main/java/com/example/guruexample/MainActivity.kt
+++ app/src/main/java/com/example/guruexample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
@@ -20,31 +21,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initRecycler()
+        Log.d("TestLog", "MainActivity")
+        btn_login.setOnClickListener(ButtonListener())
+        btn_adoptlist.setOnClickListener(ButtonListener())
 
     }
+    inner class ButtonListener: View.OnClickListener {
+        override fun onClick(v: View?) {
+            var intent = Intent()
 
-    private fun initRecycler() {
-        adoptAdapter = AdoptAdapter(this)
-        rv_profile.adapter = adoptAdapter
-
-
-        datas.apply {
-            add(AdoptData(img = R.drawable.pet_img_1_caramel, name = "카라멜", age = 2, info = "-품종 : 믹스 -성별 : 수컷 -나이 : 2세 2개월 -체중 : 5.2kg"))
-            add(AdoptData(img = R.drawable.pet_img_2_tamna, name = "탐나", age = 3, info = "-품종 : 믹스 -성별 : 수컷 -나이 : 3세 1개월 -체중 : 5.2kg"))
-            add(AdoptData(img = R.drawable.pet_img_3_tuning, name = "튜닝", age = 7, info = "-품종 : 믹스 -성별 : 암컷 -나이 : 1세 1개월 -체중 : 4.2kg"))
-            add(AdoptData(img = R.drawable.pet_img_4_timong, name = "티몽", age = 1, info = "-품종 : 푸들 -성별 : 수컷 -나이 : 3세 7개월 -체중 : 5.4kg"))
-            add(AdoptData(img = R.drawable.pet_img_5_tiyong, name = "티용", age = 3, info = "-품종 : 코리안숏헤어 -성별 : 암컷 -나이 : 3개월 -체중 : 1.6kg"))
-            add(AdoptData(img = R.drawable.pet_img_6_tellae, name = "텔레", age = 3, info = "-품종 : 코리안숏헤어 -성별 : 수컷 -나이 : 3개월 -체중 : 1.8kg"))
-            add(AdoptData(img = R.drawable.pet_img_7_tobi, name = "토비", age = 3, info = "-품종 : 코리안숏헤어 -성별 : 수컷 -나이 : 3개월 -체중 : 1.67kg"))
-
-            adoptAdapter.datas = datas
-            adoptAdapter.notifyDataSetChanged()
-
+            when (v?.id) {
+                R.id.btn_login -> {
+                    Log.d("TestLog", "LoginBtn")
+                    intent = Intent(this@MainActivity, AdoptMain::class.java)
+                }
+                R.id.btn_adoptlist -> {
+                    Log.d("TestLog", "AdoptlistBtn")
+                    intent = Intent(this@MainActivity, AdoptMain::class.java)
+                }
+            }
+            startActivity(intent)
         }
-
     }
-
-
 
 }
