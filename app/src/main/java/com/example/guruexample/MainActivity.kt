a58debf56f60d4e2f06004770c3e591cf336497a
LeeJieun <jieun6269@gmail.com>
2023-01-26 21:27:07 +0900

    버튼 클릭 화면 전환 참고 코드 주석

--- app/src/main/java/com/example/guruexample/MainActivity.kt
+++ app/src/main/java/com/example/guruexample/MainActivity.kt
@@ -2,18 +2,18 @@ package com.example.guruexample
 
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_adopt_recycler.*
 
 
+
 class MainActivity : AppCompatActivity() {
+
     lateinit var adoptAdapter: AdoptAdapter
     val datas = mutableListOf<AdoptData>()
 
@@ -21,27 +21,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("TestLog", "MainActivity")
-        btn_login.setOnClickListener(ButtonListener())
-        btn_adoptlist.setOnClickListener(ButtonListener())
 
-    }
-    inner class ButtonListener: View.OnClickListener {
-        override fun onClick(v: View?) {
-            var intent = Intent()
 
-            when (v?.id) {
-                R.id.btn_login -> {
-                    Log.d("TestLog", "LoginBtn")
-                    intent = Intent(this@MainActivity, AdoptMain::class.java)
+        inner class ButtonListener : View.OnClickListener {
+            override fun onClick(v: View?) {
+                var intent = Intent()
+
+                when (v?.id) {
+                    R.id.btn_login -> {
+                        Log.d("TestLog", "LoginBtn")
+                        intent = Intent(this@MainActivity, AdoptMain::class.java)
+                    }
+                    R.id.btn_adoptlist -> {
+                        Log.d("TestLog", "AdoptlistBtn")
+                        intent = Intent(this@MainActivity, AdoptMain::class.java)
+                    }
                 }
-                R.id.btn_adoptlist -> {
-                    Log.d("TestLog", "AdoptlistBtn")
-                    intent = Intent(this@MainActivity, AdoptMain::class.java)
-                }
+                startActivity(intent)
             }
+        }
+        /*버튼 클릭 화면 전환 참고 코드
+        val intent = Intent(this, [도착 페이지]::class.java)
+        val [버튼변수명] = findViewById<Button>(R.id.[해당버튼id])
+        buttonView.setOnClickListener {
             startActivity(intent)
         }
+        */
     }
-
 }
