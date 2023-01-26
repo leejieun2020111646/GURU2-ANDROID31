f4906efc6cbd7278479ec91211cca84fe770b52f
eyunseo <yunseo528@naver.com>
2023-01-27 00:51:18 +0900

    MainActivity 수정 -메뉴 클릭시 페이지 이동

--- app/src/main/java/com/example/guruexample/MainActivity.kt
+++ app/src/main/java/com/example/guruexample/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,31 +23,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initRecycler()
+    }
 
 
-        inner class ButtonListener : View.OnClickListener {
-            override fun onClick(v: View?) {
-                var intent = Intent()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
 
-                when (v?.id) {
-                    R.id.btn_login -> {
-                        Log.d("TestLog", "LoginBtn")
-                        intent = Intent(this@MainActivity, AdoptMain::class.java)
-                    }
-                    R.id.btn_adoptlist -> {
-                        Log.d("TestLog", "AdoptlistBtn")
-                        intent = Intent(this@MainActivity, AdoptMain::class.java)
-                    }
-                }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_login -> {
+                val intent = Intent(this, Login::class.java)
                 startActivity(intent)
+                return true
+            }
+            R.id.action_notice -> {
+                val intent = Intent(this, AdoptNotice::class.java)
+                startActivity(intent)
+                return true
             }
         }
-        /*버튼 클릭 화면 전환 참고 코드
-        val intent = Intent(this, [도착 페이지]::class.java)
-        val [버튼변수명] = findViewById<Button>(R.id.[해당버튼id])
-        buttonView.setOnClickListener {
-            startActivity(intent)
-        }
-        */
+        return super.onOptionsItemSelected(item)
     }
+
+    private fun initRecycler() {
+        adoptAdapter = AdoptAdapter(this)
+        rv_profile.adapter = adoptAdapter
+
+        datas.apply {
+            add(AdoptData(img = R.drawable.pet_img_1_caramel, name = "카라멜", age = 2, info = "-품종 : 믹스 -성별 : 수컷 -나이 : 2세 2개월 -체중 : 5.2kg"))
+            add(AdoptData(img = R.drawable.pet_img_2_tamna, name = "탐나", age = 3, info = "-품종 : 믹스 -성별 : 수컷 -나이 : 3세 1개월 -체중 : 5.2kg"))
+            add(AdoptData(img = R.drawable.pet_img_3_tuning, name = "튜닝", age = 7, info = "-품종 : 믹스 -성별 : 암컷 -나이 : 1세 1개월 -체중 : 4.2kg"))
+            add(AdoptData(img = R.drawable.pet_img_4_timong, name = "티몽", age = 1, info = "-품종 : 푸들 -성별 : 수컷 -나이 : 3세 7개월 -체중 : 5.4kg"))
+            add(AdoptData(img = R.drawable.pet_img_5_tiyong, name = "티용", age = 3, info = "-품종 : 코리안숏헤어 -성별 : 암컷 -나이 : 3개월 -체중 : 1.6kg"))
+            add(AdoptData(img = R.drawable.pet_img_6_tellae, name = "텔레", age = 3, info = "-품종 : 코리안숏헤어 -성별 : 수컷 -나이 : 3개월 -체중 : 1.8kg"))
+            add(AdoptData(img = R.drawable.pet_img_7_tobi, name = "토비", age = 3, info = "-품종 : 코리안숏헤어 -성별 : 수컷 -나이 : 3개월 -체중 : 1.67kg"))
+
+            adoptAdapter.datas = datas
+            adoptAdapter.notifyDataSetChanged()
+
+        }
+
+    }
+
+    /*버튼 클릭 화면 전환 참고 코드
+    val intent = Intent(this, [도착 페이지]::class.java)
+    val [버튼변수명] = findViewById<Button>(R.id.[해당버튼id])
+    buttonView.setOnClickListener {
+        startActivity(intent)
+    }
+    */
 }
