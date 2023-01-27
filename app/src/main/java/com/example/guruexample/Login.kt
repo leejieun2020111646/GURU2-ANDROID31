9e2739d21a5c66256eaeea2435af44b4567c0af8
LeeJieun <jieun6269@gmail.com>
2023-01-27 12:54:50 +0900

    회원가입 화면이동

--- app/src/main/java/com/example/guruexample/Login.kt
+++ app/src/main/java/com/example/guruexample/Login.kt
@@ -8,16 +8,25 @@ import kotlinx.android.synthetic.main.activity_register1.*
 
 class Login : AppCompatActivity() {
 
-    lateinit var Btn_Register: Button //회원가입 버튼 변수
+    //회원가입 버튼 변수
+    //lateinit var BtnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        Btn_Register = findViewById<Button>(R.id.btn_register)
+        /*BtnRegister = findViewById<Button>(R.id.btn_register)
 
-        //회원가입 버튼 클릭 시 페이지 이동(Register로 이동)
         Btn_Register.setOnClickListener{
             startActivity((Intent(this@Login,Register::class.java)))
+        }*/
+
+        //회원가입 버튼 클릭 시 페이지 이동(Register로 이동)
+        val BtnRegister = findViewById<Button>(R.id.btn_register)
+        BtnRegister.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
         }
+
 
 
 
