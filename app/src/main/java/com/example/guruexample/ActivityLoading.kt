57cf9cd85b1c289e03aa715b443cd92832931f0b
LeeJieun <jieun6269@gmail.com>
2023-01-30 23:58:02 +0900

    .

---
+++ app/src/main/java/com/example/guruexample/ActivityLoading.kt
@@ -0,0 +1,12 @@
+package com.example.guruexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityLoading : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
