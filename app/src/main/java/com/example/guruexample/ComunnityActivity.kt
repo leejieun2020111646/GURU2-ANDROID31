920752552619dd68166deefdc54b8c4df945ff1d
LeeJieun <jieun6269@gmail.com>
2023-01-31 18:15:49 +0900

    커뮤니티완료-xml만

---
+++ app/src/main/java/com/example/guruexample/ComunnityActivity.kt
@@ -0,0 +1,15 @@
+package com.example.guruexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ComunnityActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.comunity_screen)
+
+
+    }
+}
